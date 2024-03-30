3ffaeec66dfd5e5b0095efa827bbcaec6f6b733d
Art7578 <113849803+Art7578@users.noreply.github.com>
2024-03-30 14:20:22 +0200

    add CharInfo styles

--- src/pages/CharInfoPage/CharInfoPage.jsx
+++ src/pages/CharInfoPage/CharInfoPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCharacterInfo } from "../../service";
+import css from "./CharInfo.module.css"; // Предполагается, что у вас есть модуль CSS для этого компонента
 
 const CharInfo = () => {
   const { characterId } = useParams();
@@ -20,33 +21,45 @@ const CharInfo = () => {
   }, [characterId]);
 
   if (!character) {
-    return <div>Loading...</div>;
+    return <div className={css.loading}>Loading...</div>;
   }
 
   const { name, description, thumbnail, comics, series, stories } = character;
 
   return (
-    <div>
+    <div className={css.char_info}>
       <h1>{name}</h1>
-      <img src={`${thumbnail.path}.${thumbnail.extension}`} alt={name} />
-      <p>{description}</p>
+      <img className={css.thumbnail} src={`${thumbnail.path}.${thumbnail.extension}`} alt={name} />
+      {description ? <p>{description}</p> : <p>No description available.</p>}
       <h2>Comics</h2>
-      <ul>
-        {comics.items.map((comic, index) => (
-          <li key={index}>{comic.name}</li>
-        ))}
+      <ul className={css.comics_list}>
+        {comics.items.length > 0 ? (
+          comics.items.map((comic, index) => (
+            <li key={index} className={css.comic}>{comic.name}</li>
+          ))
+        ) : (
+          <li>No comics available.</li>
+        )}
       </ul>
       <h2>Series</h2>
-      <ul>
-        {series.items.map((serie, index) => (
-          <li key={index}>{serie.name}</li>
-        ))}
+      <ul className={css.series_list}>
+        {series.items.length > 0 ? (
+          series.items.map((serie, index) => (
+            <li key={index} className={css.serie}>{serie.name}</li>
+          ))
+        ) : (
+          <li>No series available.</li>
+        )}
       </ul>
       <h2>Stories</h2>
-      <ul>
-        {stories.items.map((story, index) => (
-          <li key={index}>{story.name}</li>
-        ))}
+      <ul className={css.stories_list}>
+        {stories.items.length > 0 ? (
+          stories.items.map((story, index) => (
+            <li key={index} className={css.story}>{story.name}</li>
+          ))
+        ) : (
+          <li>No stories available.</li>
+        )}
       </ul>
     </div>
   );
